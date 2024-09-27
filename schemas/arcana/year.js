6ca2efd8d70b7747964dac59de5806b6c88b9e4a
Ordin <18818+ordinz@users.noreply.github.com>
2024-09-27 10:01:15 +0800

    year.js now contains seed spots for +/-/<3/$

--- schemas/arcana/year.js
+++ schemas/arcana/year.js
@@ -4,6 +4,66 @@ export default {
   type: 'object',
   fields: [
     {
+      name: 'seedPositives',
+      title: 'Seed Positives',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'positives',
+      title: 'Positives',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'seedNegatives',
+      title: 'Seed Negatives',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'negatives',
+      title: 'Negatives',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'seedRelationships',
+      title: 'Seed Relationships',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'relationships',
+      title: 'Relationships',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'seedMoney',
+      title: 'Seed Money',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'money',
+      title: 'Money',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'seedHealth',
+      title: 'Seed Health',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'health',
+      title: 'Health',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
       name: 'text',
       title: 'Text',
       type: 'array',
